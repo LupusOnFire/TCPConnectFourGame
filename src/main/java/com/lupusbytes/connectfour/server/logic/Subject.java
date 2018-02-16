e56aee04c0c6f8c3d3386ff50999579115944842
LupusOnFire <bertram@lupusmedia.dk>
2018-02-16 23:56:00 +0100

    Added overload for unregister method with socket parameter

--- src/main/java/com/lupusbytes/connectfour/server/logic/Subject.java
+++ src/main/java/com/lupusbytes/connectfour/server/logic/Subject.java
@@ -5,5 +5,6 @@ import java.net.Socket;
 public interface Subject {
     void register(Socket socket, String username);
     void unregister(String username);
+    void unregister(Socket socket);
     void notifyObserver(String message);
 }
