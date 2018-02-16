7d3302b1037898bc99665cded1b1d26c519b7c36
LupusOnFire <bertram@lupusmedia.dk>
2018-02-16 18:43:28 +0100

    Implemented launch arguments

--- src/main/java/com/lupusbytes/connectfour/Main.java
+++ src/main/java/com/lupusbytes/connectfour/Main.java
@@ -1,42 +1,44 @@
 package com.lupusbytes.connectfour;
 
 import com.lupusbytes.connectfour.client.CommandlineClient;
+import com.lupusbytes.connectfour.client.IClient;
 import com.lupusbytes.connectfour.client.logic.controller.Controller;
 import com.lupusbytes.connectfour.server.logic.ConnectionBroker;
 import com.lupusbytes.connectfour.server.logic.Lobby;
 import com.lupusbytes.connectfour.server.logic.entity.Board;
 import com.lupusbytes.connectfour.server.logic.entity.Slot;
+import com.sun.org.apache.xpath.internal.SourceTree;
+import org.apache.commons.cli.*;
 
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        //TODO use Apache commons to have more sane CLI controls
-        if (args.length > 0) {
-            if (args[0].equals("--com.lupusbytes.connectfour.server") || args[0].equals("-s")) {
-                Integer serverPort = null;
-                try {
-                    serverPort = Integer.parseInt(args[1]);
-                } catch (Exception e) {
-                    System.out.println("Missing com.lupusbytes.connectfour.server port integer");
-                    return;
-                }
-                Lobby engine = new Lobby();
-                ConnectionBroker connectionBroker = new ConnectionBroker(engine, serverPort);
-                Thread connectionThread = new Thread(connectionBroker);
-                connectionThread.start();
-            } else if (args[0].equals("--com.lupusbytes.connectfour.client") || args[0].equals("-c")) {
-                if (args.length > 1 && (args[1].equals("-h") || args[1].equals("--hotseat"))) {
-                    hotSeat();
-                } else {
-                    Controller controller = new Controller();
-                    CommandlineClient cli = new CommandlineClient(controller);
-                    cli.setRemoteServer();
-                    if (cli.joinServer()){
-                        cli.clientInput();
-                    }
-                }
+        int port = 32450;
+
+        try {
+            Options options = new Options();
+            options.addOption("s", "server",false, "Start server");
+            options.addOption("c", "client",false, "Start client");
+            options.addOption("h","headless",false, "Start headless client");
+            options.addOption("p", "port",true, "Port");
+            CommandLineParser cmdParser = new GnuParser();
+            CommandLine cmd = cmdParser.parse(options, args);
+
+            if (cmd.hasOption("p"))
+                port = Integer.parseInt(cmd.getOptionValue("p"));
+
+            // Start client or server, never more than one
+            if (cmd.hasOption("c") && !cmd.hasOption("h")) {
+                System.out.println("JavaFX client started");
+            } else if (cmd.hasOption("h")) {
+                System.out.println("CLI client starting");
+            } else if (cmd.hasOption("s")) {
+                System.out.println("Server starting");
+                startServer(port);
             }
+        } catch (ParseException e) {
+            System.out.println("Parsing failed. Reason: " + e.getMessage());
         }
     }
     public static void hotSeat(){
@@ -53,6 +55,25 @@ public class Main {
             input = scanner.nextLine();
             row = Integer.parseInt(input);
             board.putPiece(row, Slot.PLAYER2);
+        }
+    }
+    public static void startServer(int port) {
+        Lobby engine = new Lobby();
+        ConnectionBroker connectionBroker = new ConnectionBroker(engine, port);
+        Thread connectionThread = new Thread(connectionBroker);
+        connectionThread.start();
+    }
+
+    public static void startGuiClient() {
+
+    }
+
+    public static void startCliClient() {
+        Controller controller = new Controller();
+        IClient cliClient = new CommandlineClient(controller);
+        cliClient.setRemoteServer();
+        if (cliClient.joinServer()){
+            cliClient.clientInput();
         }
     }
 }
